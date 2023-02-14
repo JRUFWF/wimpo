1c77677c721469ef6a4e4d7184f8fc2bd577a732
wenjun fan <wenjun.fan@purina.nestle.com>
2023-02-14 09:11:48 -0800

    remove captcha, go tracking result directly

--- app.js
+++ app.js
@@ -17,27 +17,48 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use('/', indexRouter);
-app.post('/captcha', function(req, res) {
+app.post('/tracking-result', function(req, res) {
     if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null)
     {
         return res.json({"responseError" : "captcha error"});
-        }
-        const secretKey = "174faff8fbc769e94a5862391ecfd010";
-        const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
+    }
+    const secretKey = "174faff8fbc769e94a5862391ecfd010";
+    const verificationURL = "https://www.google.com/recaptcha/api/siteverify?secret=" + secretKey + "&response=" + req.body['g-recaptcha-response'] + "&remoteip=" + req.connection.remoteAddress;
 
-        request(verificationURL,function(error,response,body) {
-            if(body) {
-                body = JSON.parse(body);
-                if(body.success !== undefined && !body.success) {
-                    return res.json({"responseError" : "Failed captcha verification"});
-                }
-                window.location.replace("/tracking-result");
-                res.json({"responseSuccess" : "Success"});
-            } else {
-                return res.json({"responseError" : error});
+    request(verificationURL,function(error,response,body) {
+        if(body) {
+            body = JSON.parse(body);
+
+            if(body.success !== undefined && !body.success) {
+                // return res.json({"responseError" : "Failed captcha verification"});
             }
-        });
-    });
+            var result = [];
+            if (req.body['order-number'] && req.body['zipcode']) {
+                result = [
+                    {
+                        orderDate: '11/16/2024',
+                        shippedDate: '11/26/2024',
+                        name: 'PPVS FORTIFLORA CNNE SUPL 6*30 CNT N9 US',
+                        ordered: 200,
+                        shipped: 200,
+                        tracking: 'https://www.fedex.com/fedextrack/?trknbr=593987129293',
+                    },
+                    {
+                        orderDate: '11/16/2024',
+                        shippedDate: null,
+                        name: 'PPVD HA FLN 1X8LB US',
+                        ordered: 200,
+                        shipped: 0,
+                        tracking: null,
+                    },
+                ];
+            }
+            res.status(200).render('./pages/result', { page: 'Tracking Result', data: result});
+        } else {
+            return res.json({"responseError" : error});
+        }
+     });
+});
 
 // catch 404 and forward to error page
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -5,32 +5,6 @@ router.get('/', (req, res, next) => {
   res.render('index', { page: 'Home' });
 });
 
-router.get('/captcha', (req, res, next) => {
-  res.render('index', { page: 'Recaptcha' });
-});
-
-router.get('/tracking-result', (req, res, next) => {
-  var result = [
-    {
-      orderDate: '11/16/2024',
-      shippedDate: '11/26/2024',
-      name: 'PPVS FORTIFLORA CNNE SUPL 6*30 CNT N9 US',
-      ordered: 200,
-      shipped: 200,
-      tracking: 'https://www.fedex.com/fedextrack/?trknbr=593987129293',
-    },
-    {
-      orderDate: '11/16/2024',
-      shippedDate: null,
-      name: 'PPVD HA FLN 1X8LB US',
-      ordered: 200,
-      shipped: 0,
-      tracking: null,
-    },
-  ];
-  res.render('index', { page: 'Tracking Result', data: result} );
-});
-
 router.get('/faq', (req, res, next) => {
   var faqs = [
     {
@@ -48,6 +22,5 @@ router.get('/faq', (req, res, next) => {
   ];
   res.render('index', { page: 'FAQ', data: faqs });
 });
-
 
 module.exports = router;
